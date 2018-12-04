2d2f94c7cadda8da105c0e92e798d14ce8755004
debiancc <wtf@debiancc.me>
2018-12-05 00:01:09 +0800

    feat(user): Add user list example

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,48 @@ import { Component, Vue } from 'nuxt-property-decorator'
   components: { Logo }
 })
 export default class Landing extends Vue {
+  title = 'Landing#Index'
+
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'keyword', name: 'keyword', content: `${this.title} keywords` },
+        {
+          hid: 'description',
+          name: 'description',
+          content: `${this.title} description`
+        }
+      ]
+    }
+  }
+  asyncData() {
+    // mock async data like backend template variable
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        return resolve({
+          users: [
+            { id: 0, name: 'user-0' },
+            { id: 1, name: 'user-1' },
+            { id: 2, name: 'user-2' },
+            { id: 3, name: 'user-3' }
+          ]
+        })
+      }, 1000 * 2)
+    })
+  }
+
   render(h) {
-    return <h1>Landing#Index</h1>
+    return (
+      <div>
+        <h1>Landing#Index</h1>
+        <ul>
+          <li>
+            <nuxt-link to="/user/1">User 1</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    )
   }
 }
 </script>
-
-<style>
-</style>
---
+++ pages/user/_id.vue
@@ -0,0 +1,17 @@
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class UserDetail extends Vue {
+  render(h) {
+    return (
+      <div>
+        <h2>
+          User#
+          {this.$route.params.id}
+        </h2>
+      </div>
+    )
+  }
+}
+</script>
